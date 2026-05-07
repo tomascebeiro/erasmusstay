01b81949b07da21fc46cac0cd42045f572304bc0
Tomás Cebeiro Cabo <a25tomascc@iessanclemente.net>
2026-05-07 04:17:56 +0000

    rutas de login, registro y perfil del usuario

--- backend/src/housing/urls.py
+++ backend/src/housing/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import AnuncioViewSet, ValoracionViewSet, RegisterView
+from .views import AnuncioViewSet, ValoracioViewSet, RegistroView, LoginView, MiPerfilView
 
 router = DefaultRouter()
 router.register(r'anuncios', AnuncioViewSet)
-router.register(r'valoraciones', ValoracionViewSet)
+router.register(r'valoraciones', ValoracioViewSet)
 
 urlpatterns = router.urls + [
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', RegistroView.as_view(), name='registro'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('me/', MiPerfilView.as_view(), name='mi-perfil'),
 ]
